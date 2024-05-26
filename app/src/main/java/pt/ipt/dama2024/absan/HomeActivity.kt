6dd35955a262a32a7f6faf22041e139b0a57031a
RomaoFilipe <romaofilipe1999@gmail.com>
2024-05-26 23:16:24 +0100

    Creation of Home Activity and Manifest correction

---
+++ app/src/main/java/pt/ipt/dama2024/absan/HomeActivity.kt
@@ -0,0 +1,12 @@
+package pt.ipt.dama2024.absan
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
